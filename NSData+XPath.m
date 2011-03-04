428e76d2a7464fb9b26a7fc1d698dcd965eb8a8b
Steven Fusco <sfusco@cibotechnology.com>
2011-03-04 00:03:48 -0500

    code cleanup

--- NSData+XPath.m
+++ NSData+XPath.m
@@ -22,54 +22,6 @@
 #import <libxml/xpath.h>
 #import <libxml/xpathInternals.h>
 
-/**
- * print_xpath_nodes:
- * @nodes:		the nodes set.
- * @output:		the output file handle.
- *
- * Prints the @nodes content to @output.
- */
-/*void
-print_xpath_nodes(xmlNodeSetPtr nodes, FILE* output) {
-    xmlNodePtr cur;
-    int size;
-    int i;
-    
-    assert(output);
-    size = (nodes) ? nodes->nodeNr : 0;
-    
-    fprintf(output, "Result (%d nodes):\n", size);
-    for(i = 0; i < size; ++i) {
-        assert(nodes->nodeTab[i]);
-        
-        if(nodes->nodeTab[i]->type == XML_NAMESPACE_DECL) {
-            xmlNsPtr ns;
-            
-            ns = (xmlNsPtr)nodes->nodeTab[i];
-            cur = (xmlNodePtr)ns->next;
-            if(cur->ns) { 
-                fprintf(output, "= namespace \"%s\"=\"%s\" for node %s:%s\n", 
-                ns->prefix, ns->href, cur->ns->href, cur->name);
-            } else {
-                fprintf(output, "= namespace \"%s\"=\"%s\" for node %s\n", 
-                ns->prefix, ns->href, cur->name);
-            }
-        } else if(nodes->nodeTab[i]->type == XML_ELEMENT_NODE) {
-            cur = nodes->nodeTab[i];   	    
-            if(cur->ns) { 
-                    fprintf(output, "= element node \"%s:%s\"\n", 
-                cur->ns->href, cur->name);
-            } else {
-                    fprintf(output, "= element node \"%s\"\n", 
-                cur->name);
-            }
-        } else {
-            cur = nodes->nodeTab[i];    
-            fprintf(output, "= node \"%s\": type %d\n", cur->name, cur->type);
-        }
-    }
-}*/
-
 #define nsxmlstr( S ) [NSString stringWithCString:(const char *) S encoding:NSUTF8StringEncoding]
 
 
@@ -77,9 +29,6 @@ print_xpath_nodes(xmlNodeSetPtr nodes, FILE* output) {
 
 - (void) findXPath:(NSString*)query usingNamespaces:(NSDictionary*)namespaces executeBlock:(XPathResultBlock)block
 {
-    // TODO: move init and cleanup somewhere else
-    xmlInitParser();
-    
     xmlDocPtr doc = xmlReadMemory([self bytes], [self length], "", NULL, XML_PARSE_RECOVER);
 	
     if (doc == NULL)
@@ -163,8 +112,6 @@ print_xpath_nodes(xmlNodeSetPtr nodes, FILE* output) {
     xmlXPathFreeObject(xpathObj);
     xmlXPathFreeContext(xpathCtx); 
     xmlFreeDoc(doc);
-    
-    xmlCleanupParser();
 }
 
 - (NSArray*) findXPath:(NSString *)query usingNamespaces:(NSDictionary *)namespaces
